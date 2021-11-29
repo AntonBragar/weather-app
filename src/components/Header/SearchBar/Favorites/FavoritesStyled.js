1bece32c7f2e2aa61c5a0c6d3f2d295e187359b7
nazarsilveroff <nazar91187@gmail.com>
2021-11-29 13:57:47 +0200

    fix favorite, finish currentDate styles

--- src/components/Header/SearchBar/Favorites/FavoritesStyled.js
+++ src/components/Header/SearchBar/Favorites/FavoritesStyled.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 
 export const FavoriteWrapper = styled.div`
-  
-  .favoriteBox{
+
+  .favoriteBox {
     display: flex;
     margin: 15px auto 0;
     width: 280px;
+    align-items: center;
+    justify-content: center;
     @media (min-width: 768px) {
       width: 500px;
     }
@@ -16,58 +18,73 @@ export const FavoriteWrapper = styled.div`
       width: 520px;
     }
   }
-  
+
   .citiesList {
     display: flex;
     overflow: scroll;
     width: inherit;
-    border-radius: 20px;
-  }
-  .citiesList::-webkit-scrollbar {
-      display: none;
-    }
+    padding-bottom: 5px;
     
+    ::-webkit-scrollbar {
+      width: 0px;
+      height: 5px;
+      border-radius: 4px;
+      background-color: rgba(255, 255, 255, 0.15);
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: rgba(0, 0, 0, 0.2);
+      border-radius: 4px;
+      height: 15px;
+    }
+
+  }
+
   .cityItem {
     display: flex;
     align-items: center;
     justify-content: center;
     height: 28px;
-    //width: 74px;
     background: rgba(16, 33, 54, 0.5);
     font-size: 12px;
     color: #FFFFFF;
     border-radius: 20px;
     margin: 5px;
+    
   }
-  
+
   .cityItem:hover {
-    font-size: 20px;
-    font-weight: bold;
-    height: 40px;
+    height: 38px;
+
+    .cityTitle {
+      font-size: 18px;
+    }
+
     .deleteItem > svg {
-      height: 13px;
-      width: 13px;
+      height: 15px;
+      width: 15px;
     }
   }
-  
+
   .cityTitle {
-    padding-left: 13px;
+    padding: 0px 10px;
     font-size: 15px;
     color: #FFFFFF;
   }
-  
+
   .deleteItem {
+    padding-right: 10px;
     background-color: transparent;
     border: none;
     fill: #FFFFFF;
-    padding: 10px;
   }
-  
+
   .rightArrow {
+    margin-left: 20px;
     background: rgba(255, 255, 255, 0.2);
     border-radius: 50%;
     border: none;
-    height: 43px;
-    width: 43px;
+    height: 38px;
+    width: 38px;
   }
 `
