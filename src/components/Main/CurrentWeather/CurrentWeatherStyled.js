271aee55fe3541a58a4326865df7bef2e038fe2c
nazarsilveroff <nazar91187@gmail.com>
2022-01-18 13:25:52 +0200

    add styles for CurrentWeatherStyled.js component for tablets breakpoints

--- src/components/Main/CurrentWeather/CurrentWeatherStyled.js
+++ src/components/Main/CurrentWeather/CurrentWeatherStyled.js
@@ -3,12 +3,12 @@ import {createWeatherIconSrc} from "../../../utils/createWeatherIconSrc";
 
 
 export const WeatherImageContainer = styled.div`
-  background-image: url(${({ icon }) => createWeatherIconSrc(icon)});
+  background-image: url(${({icon}) => createWeatherIconSrc(icon)});
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-    width: 70px;
-    height: 50px;
+  width: 70px;
+  height: 50px;
 `
 
 export const CurrentWeatherWrapper = styled.div`
@@ -19,6 +19,10 @@ export const CurrentWeatherWrapper = styled.div`
     background: #102136;
     box-shadow: 0px 5px 33px rgba(16, 33, 54, 0.6);
     border-radius: 34px;
+    @media (min-width: 768px) {
+      width: 500px;
+      height: 300px;
+    }
   }
 
   .itemsBox {
@@ -28,6 +32,9 @@ export const CurrentWeatherWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    @media (min-width: 768px) {
+      height: inherit;
+    }
   }
 
   .weatherAndLocation {
@@ -36,19 +43,32 @@ export const CurrentWeatherWrapper = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
+    @media (min-width: 768px) {
+      margin-top: 0;
+    }
   }
-  
+
 
   .location {
     font-size: 16px;
     line-height: 19px;
     color: #767D85;
+    @media (min-width: 768px) {
+      font-size: 18px;
+      line-height: 22px;
+    }
   }
 
   .temperature {
     margin-top: 25px;
     display: flex;
     align-items: center;
+    @media (min-width: 768px) {
+      margin-top: 0px;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    }
   }
 
   .tempBigNum {
@@ -59,21 +79,36 @@ export const CurrentWeatherWrapper = styled.div`
     line-height: 54px;
     letter-spacing: 0.04em;
     color: #FFFFFF;
+    @media (min-width: 768px) {
+      margin-right: 0;
+      font-weight: 300;
+      font-size: 90px;
+      line-height: 108px;
+    }
   }
-  .tempList{
+
+  .tempList {
     display: flex;
-    
+
   }
+
   .tempItem {
     display: flex;
     flex-direction: column;
-    
-    :first-child{
+
+    :first-child {
       padding-right: 5px;
       border-right: 1px solid #27384A;
+      @media (min-width: 768px) {
+        padding-right: 10px;
+      }
     }
-    :last-child{
+
+    :last-child {
       padding-left: 5px;
+      @media (min-width: 768px) {
+        padding-left: 10px;
+      }
     }
   }
 
@@ -83,6 +118,10 @@ export const CurrentWeatherWrapper = styled.div`
     font-size: 14px;
     line-height: 17px;
     color: #767D85;
+    @media (min-width: 768px) {
+      font-size: 18px;
+      line-height: 22px;
+    }
   }
 
   .tempNum {
@@ -91,5 +130,9 @@ export const CurrentWeatherWrapper = styled.div`
     font-size: 16px;
     line-height: 19px;
     color: #FFFFFF;
+    @media (min-width: 768px) {
+      font-size: 18px;
+      line-height: 22px;
+    }
   }
 `
